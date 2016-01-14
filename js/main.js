a0727200b299d86e32eb32fe715bd812c2837c51
Ibrahim Itani <andrahim@gmail.com>
2016-01-13 19:11:35 -0800

    initial scaffold

---
+++ js/main.js
@@ -0,0 +1,27 @@
+function play1()
+{
+document.getElementById('audio1').play();
+}
+
+function play2()
+{
+document.getElementById('audio2').play();
+}
+
+function play3()
+{
+document.getElementById('audio3').play();
+}
+
+function play4()
+{
+document.getElementById('audio4').play();
+}
+
+function stopMusic()
+{
+document.getElementById('audio1').pause();
+document.getElementById('audio2').pause();
+document.getElementById('audio3').pause();
+document.getElementById('audio4').pause();
+}
